ef25261bb965bfb93ae1f6a33fc15e63e3bad720
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-27 09:13:31 +0530

    conditional length change

--- components/home/EditorialPart.tsx
+++ components/home/EditorialPart.tsx
@@ -2,9 +2,14 @@ import Link from 'next/link';
 import { Flex, Text, Link as ChakraLink } from '@chakra-ui/react';
 import { editorialContent } from '@/data/index';
 import { appAccentColor, appSecondaryColor } from '@/styles/colors';
+import { useWindowSize } from 'hooks/useWindowSize';
 
 export default function EditorialPart() {
-  const content = editorialContent.content.substring(0, 700);
+  const { width } = useWindowSize();
+
+  const contentLength = width ? (width > 700 ? 700 : 300) : 700;
+
+  const content = editorialContent.content.substring(0, contentLength);
 
   return (
     <Flex
@@ -20,14 +25,14 @@ export default function EditorialPart() {
           fontSize='2xl'
           textDecoration='underline'
           fontWeight='medium'
-          mb={3}
+          mb={5}
         >
           {editorialContent.title}
         </Text>
         <Text fontSize='xl'>{content} . . .</Text>
       </Flex>
       <Link href={`/content/${editorialContent.id}`} passHref>
-        <ChakraLink mt={3} color={appAccentColor} fontSize='xl'>
+        <ChakraLink mt={5} color={appAccentColor} fontSize='xl'>
           Read More
         </ChakraLink>
       </Link>
---
+++ hooks/useWindowSize.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+interface WindowSize {
+  width: number | undefined;
+  height: number | undefined;
+}
+
+export function useWindowSize() {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const handleResize = () => {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      handleResize();
+
+      return () => window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+  return windowSize;
+}
